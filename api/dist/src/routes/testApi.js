7a41a961d73dd5436c406331b34dc769f02775ac
Phil Keselman <pkeselman17@gmail.com>
2020-03-03 21:56:41 -0500

    Connect client to api and test connection

---
+++ api/dist/src/routes/testApi.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const router = express_1.Router();
+router.get("/", (req, res, next) => {
+    res.send("API WORKING!");
+});
+exports.default = router;
+//# sourceMappingURL=testApi.js.map
--- api/src/index.ts
+++ api/src/index.ts
@@ -3,7 +3,7 @@ import app from '@server';
 import logger from '@shared/Logger';
 
 // Start the server
-const port = Number(process.env.PORT || 3000);
+const port = Number(process.env.PORT || 9000);
 app.listen(port, () => {
     logger.info('Express server started on port: ' + port);
 });
